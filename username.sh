9fc23924d4d5a4716f07d9848031e58cf0778467
Devin Keller <dkeller@chapman.edu>
2020-11-11 10:38:41 -0800

    updates regex

--- username.sh
+++ username.sh
@@ -1,1 +1,17 @@
+#! /bin/bash
+# username.sh
+# Devin Keller
 
+echo"Enter a username: "
+echo "Use lower case characters, digits and underscore"
+echo "It must start with a lower case character"
+echo "It must contain at least 3 but not more than 12 characters"
+
+read USERNAME
+while echo "$USERNAME" | egrep -v "^[a-z][0-9]{5}$"> /dev/null 2>&1
+do
+	echo"You must enter a valid username code -five digits only!"
+	enter "Enter a five-digit Zip code: "
+	read USERNAME
+done
+echo "Thank you"
